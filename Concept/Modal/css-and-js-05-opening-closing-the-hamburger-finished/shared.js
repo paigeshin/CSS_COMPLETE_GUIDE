3ed950768cb1bc4a4981c1c5c0c1ca8ee18a6cb1
shin <proscus0706@gmail.com>
2019-12-27 20:18:10 +0900

    modal section complete

--- Concept/Modal/css-and-js-05-opening-closing-the-hamburger-finished/shared.js
+++ Concept/Modal/css-and-js-05-opening-closing-the-hamburger-finished/shared.js
@@ -5,16 +5,20 @@ const selectPlanButtons = document.querySelectorAll(".plan button");
 const toggleButton = document.querySelector('.toggle-button');
 const mobileNav = document.querySelector('.mobile-nav');
 
+console.dir(backdrop.style['background-image']);
+
 // console.dir(backdrop);
 // 각각의 Plan 버튼을 누르면 각각의 modal과 backgdrop이 보여짐
-for (let i = 0; i < selectPlanButtons.length; i++) {
-    selectPlanButtons[i].addEventListener("click", () => {
-        // modal.style.display = "block";
-        // backdrop.style.display = "block";
-        // modal.className = "open";
-        modal.classList.add('open'); //복수의 같은 element들이 있으면 classList를 사용하자
-        backdrop.classList.add('open');
-    });
+if (selectPlanButtons) {
+    for (let i = 0; i < selectPlanButtons.length; i++) {
+        selectPlanButtons[i].addEventListener("click", () => {
+            // modal.style.display = "block";
+            // backdrop.style.display = "block";
+            // modal.className = "open";
+            modal.classList.add('open'); //복수의 같은 element들이 있으면 classList를 사용하자
+            backdrop.classList.add('open');
+        });
+    }
 }
 
 // backdrop을 다시 누르면, modal과 backdrop이 사라짐. 그리고 backdrop을 누르면 navbar도 사라짐
@@ -24,20 +28,26 @@ backdrop.addEventListener("click", () => {
 });
 
 // modal no button을 누르면 backdrop과 modal이 사라짐.
-modalNoButton.addEventListener("click", closeModal);
+if (modalNoButton) {
+    modalNoButton.addEventListener("click", closeModal);
+}
 
 // toggle button을 누르면 mobile nav가 보여짐
 toggleButton.addEventListener('click', () => {
     // mobileNav.style.display = 'block';
     // backdrop.style.display = 'block';
+
     mobileNav.classList.add('open');
     backdrop.classList.add('open');
+
 });
 
 // 모달과 backdrop을 지워줌
 function closeModal() {
     // backdrop.style.display = "none";
     // modal.style.display = "none";
-    modal.classList.remove('open'); //복수의 같은 element들이 있으면 classList를 사용하지
+    if(modal){
+        modal.classList.remove('open'); //복수의 같은 element들이 있으면 classList를 사용하지
+    }
     backdrop.classList.remove('open');
 }
